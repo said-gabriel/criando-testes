c4df3f5056b84cdcef7575f6b1609eae1ff88292
said-gabriel <said-gabriel>
2020-10-01 16:54:24 -0300

    ClienteTeste concluído com sucesso closes #1

---
+++ Pedido/src/test/java/br/ufes/testes/ProdutoTeste.java
@@ -0,0 +1,57 @@
+package br.ufes.testes;
+
+import br.ufes.model.Cliente;
+import br.ufes.model.Produto;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProdutoTeste {
+    
+    public ProdutoTeste() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void CT001(){
+        Produto produto = new Produto("Camisa rosa", 20.00, 300);
+        String nomeEsperado = "Camisa rosa";
+        double valorEsperado = 20.00;
+        double quantidadeEsperada = 300;
+        assertEquals(nomeEsperado, produto.getNome());
+        assertEquals(valorEsperado, produto.getValorUnitario());
+        assertEquals(quantidadeEsperada, produto.getQuantidade());
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void CT002(){
+        Produto produto = new Produto("Camisa rosa", 20.00, 300);
+        String nomeEsperado = "Camisa rosa";
+        double valorEsperado = 20.00;
+        double quantidadeEsperada = 300;
+        assertEquals(nomeEsperado, produto.getNome());
+        assertEquals(valorEsperado, produto.getValorUnitario());
+        assertEquals(quantidadeEsperada, produto.getQuantidade());
+    }
+
+
+}
